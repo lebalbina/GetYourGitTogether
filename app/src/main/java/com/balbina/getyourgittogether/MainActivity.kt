89935ba5dc7810a7552c0aa7c8e9634577ff7d16
malwina pinecone <malwina.pinecone@gmail.com>
2021-02-07 22:43:03 +0100

    first commit

---
+++ app/src/main/java/com/balbina/getyourgittogether/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.balbina.getyourgittogether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
