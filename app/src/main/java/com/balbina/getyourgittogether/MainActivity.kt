4bf61de2395803c739d0337be9065174ff87ac4a
malwina pinecone <malwina.pinecone@gmail.com>
2021-02-10 22:51:59 +0100

    add buttons for showing toasts

--- app/src/main/java/com/balbina/getyourgittogether/MainActivity.kt
+++ app/src/main/java/com/balbina/getyourgittogether/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.balbina.getyourgittogether
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToastBtn.setOnClickListener {
+            Toast.makeText(this, "Show toast nr 1", Toast.LENGTH_SHORT).show()
+        }
+
+        showToastBtn2.setOnClickListener {
+            Toast.makeText(this, "Show toast nr 2", Toast.LENGTH_SHORT).show()
+        }
+
+        showToastBtn3.setOnClickListener {
+            Toast.makeText(this, "Show toast nr 3", Toast.LENGTH_SHORT).show()
+        }
     }
 }
