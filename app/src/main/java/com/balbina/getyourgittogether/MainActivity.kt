52217c618b050de716b61ffedd0c7b93f7534919
malwina pinecone <malwina.pinecone@gmail.com>
2021-03-24 23:08:50 +0100

    toasts should be long

--- app/src/main/java/com/balbina/getyourgittogether/MainActivity.kt
+++ app/src/main/java/com/balbina/getyourgittogether/MainActivity.kt
@@ -24,6 +24,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun mergeConflictedMethod(text: String) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
     }
 }
